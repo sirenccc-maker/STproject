887d8e6fd1a033dc4fa9a1f5cb85ffb839a0fe6b
yw-3408 <yw3408@columbia.edu>
2019-12-07 20:22:39 +0000

    Add map

--- ST/views.py
+++ ST/views.py
@@ -10,3 +10,8 @@ def all_squirrel(request):
             }
     return render(request,'ST/all.html',context)
 # Create your views here.
+
+def show_map(request):
+    squirrels=Squirrel.objects.all()
+    context={'squirrels':squirrels}
+    return render(request, 'ST/show_map.html', context)
