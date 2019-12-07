91e93a8246d561d3501e8ac437892adbc2bc0e0a
yw-3408 <yw3408@columbia.edu>
2019-12-07 22:37:49 +0000

    add stats html url

--- ST/urls.py
+++ ST/urls.py
@@ -4,5 +4,6 @@ urlpatterns=[
         path('',views.all_squirrel),
         path('<squirrel_id>/',views.edit_squirrel),
         path('add/',views.add_squirrel),
+        path('stats/',views.stats),
 ]
 
--- ST/views.py
+++ ST/views.py
@@ -48,4 +48,21 @@ def show_map(request):
 
 def stats(request):
     squirrels=Squirrel.objects.all()
+    squirrels_n=squirrels.count()
+    context={
+            'adult_squirrels':squirrels.filter(Age='Adult').count(),
+            'juvenile_squirrels':squirrels.filter(Age='Juvenile').count(),
+            'gray_squirrels':squirrels.filter(Primary_Fur_Color='Gray').count(),
+            'cinna_squirrels':squirrels.filter(Primary_Fur_Color='Cinnamon').count(),
+            'black_squirrels':squirrels.filter(Primary_Fur_Color='Black').count(),
+            'Ground_Plane':squirrels.filter(LOCATION='Ground_Plane').count(),
+            'Above_Ground':squirrels.filter(LOCATION='Above_Ground').count(),
+            'Running_True':squirrels.filter(Running=True).count(),
+            'Running_False':squirrels.filter(Running=False).count(),
+            "Tail_flag_True":squirrels.filter(Tail_flags=True).count(),
+            "Tail_flag_False":squirrels.filter(Tail_flags=False).count(),
+            'Moan_True':squirrels.filter(Moans=True).count(),
+            "Moan_False":squirrels.filter(Moans=False).count(),
+            }
+    return render(request, 'ST/stats.html', context)
 
